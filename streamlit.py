d99a974f646332bc4a2ba7f208f34b8efe161979
rsujithra91 <rsujithra91@gmail.com>
2026-06-10 13:39:20 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,6 @@
 import streamlit as st
 st.title("Hai")
 name=st.text_input("Enter your name")
+age=st.number_input("Enter your age")
 if st.button("Submit"):
-  st.write(f"Hello,{name}")
+  st.write(f"Hello,{name},age is {age}")
