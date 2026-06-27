e2963ce8da6d9e52e0305236d36377cc035a4104
rsujithra91 <rsujithra91@gmail.com>
2026-06-27 10:13:09 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,5 +2,26 @@ import streamlit as st
 st.title("Hai")
 name=st.text_input("Enter your name")
 age=st.number_input("Enter your age")
+def get_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+img = get_base64("background.jpg")
+
+st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url("data:image/jpg;base64,{robo}");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 if st.button("Submit"):
   st.write(f"Hello,{name},age is {age}")
