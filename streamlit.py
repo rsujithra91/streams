b5463a569596fd5c908b22e92ae332bb77b19b8e
rsujithra91 <rsujithra91@gmail.com>
2026-06-27 10:17:42 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,13 +1,17 @@
 import streamlit as st
+import base64
 st.title("Hai")
 name=st.text_input("Enter your name")
 age=st.number_input("Enter your age")
+
+
+# Function to convert image to base64
 def get_base64(file):
     with open(file, "rb") as f:
         data = f.read()
     return base64.b64encode(data).decode()
 
-img = get_base64("robo.jpg")
+img = get_base64("background.jpg")
 
 st.markdown(
     f"""
@@ -23,5 +27,6 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+
 if st.button("Submit"):
   st.write(f"Hello,{name},age is {age}")
